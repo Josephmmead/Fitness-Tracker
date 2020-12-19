8cb221db97f3371ba6c870eee87af3cbb17773ec
Joseph Mead <josephmmead@gmail.com>
2020-12-18 22:17:03 -0500

    finished models and html routes, working on finishing api routes

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
+const logger = require("morgan");
+
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
@@ -18,9 +20,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useUnifiedTopology: true,
 });
 
+
 // routes
-app.use(require("./routes/html.js"))
-app.use(require("./routes/api.js"));
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
